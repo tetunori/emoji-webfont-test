f2a0b784ab0a9a68d9c64e74276116978481af53
tetunori <tetunori@gmail.com>
2024-12-24 16:11:38 +0900

    Add sample code

---
+++ mySketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    textFont('fluentcoloremoji');
+    textSize(width/10);
+    textAlign(CENTER, CENTER)
+}
+
+function draw() {
+  background(20);
+  text('😀😁😂🤣😄😎', width/2, height/3);
+  text('🎁🦌🎅🏻🔔🎄⭐', width/2, 2*height/3);
+}
+
+
